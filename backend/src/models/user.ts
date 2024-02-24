9e2685f9f7a89ae8a248b14541f1dadfa8a7e064
Nichole McGrew <nicholemcgrew@gmail.com>
2024-02-24 06:35:20 -0600

    created user schema

---
+++ backend/src/models/user.ts
@@ -0,0 +1,26 @@
+import {Schema, model, models, Model} from 'mongoose'
+
+interface IUser{
+    _id?: string;
+    email: string;
+    password: string;
+}
+
+const userSchema = new Schema<IUser>(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            index: true,
+        }, 
+        password: { type: String, required: true, select: false }
+    },
+    {
+        timestamps: true,
+        autoIndex: true,
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true },
+    }
+);
