ae4aec2a65cf9600ba4ebc303dd234830a835ba6
Nichole McGrew <nicholemcgrew@gmail.com>
2024-02-24 06:29:06 -0600

    connected mongoDB

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ dotenv.config();
 import express, {Request, Response, Application} from 'express'
 import cors from 'cors'
 import helmet from 'helmet';
+import {connect} from 'mongoose'
 
 const app: Application = express();
 
@@ -14,6 +15,14 @@ app.use(helmet());
 app.all('*', (req: Request, res: Response) => {
     res.status(404).json({message: 'The route you requested is not found'})
 })
+
+const runDB = async () => {
+    connect(process.env.MONGODB_URI as string)
+    .then(() => console.log("DB connected successfully"))
+    .catch(() => console.log("DB not connected"))
+}
+
+runDB()
 
 const PORT = (process.env.PORT as unknown as number) || 5000;
 
